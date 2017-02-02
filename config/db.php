fdd6781658f18c341913734d9d37e2e2eed4a20d
Nikolya <k_m_i@ukr.net>
2017-02-02 08:07:09 +0200

    db

--- config/db.php
+++ config/db.php
@@ -1,9 +1,25 @@
 <?php
 
-return [
+$db = [];
+
+$db = [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=ficus_klimkin',
-    'username' => 'root',
+    'dsn' => 'mysql:host=localhost;dbname=bd_name',
+    'username' => '',
     'password' => '',
     'charset' => 'utf8',
+    'enableSchemaCache' => true,
+	'schemaCache' => 'yii\caching\FileCache',
 ];
+
+if (YII_ENV_DEV) {
+	$db = [
+	    'class' => 'yii\db\Connection',
+	    'dsn' => 'mysql:host=localhost;dbname=bd_name',
+	    'username' => 'root',
+	    'password' => '',
+	    'charset' => 'utf8',
+	];
+}
+
+return $db;
